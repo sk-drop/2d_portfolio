3657af4e321826704c0b8874e05add7c80b0977c
sk-go <kaunsasha@proton.me>
2024-05-23 16:56:07 +0200

    init

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const scalefactor = 3
+
+export const dialogueData = {
+    grab1: "Computer Science",
+    grab2: "Psychology",
+    table: "Current Ideas",
+    tv1: "You are in a Simulation"
+}
